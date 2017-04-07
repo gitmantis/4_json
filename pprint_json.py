174713f9409972cac1ccb40e17d43d0d32ade537
gitmantis <kolesnikov.andrey@gmail.com>
2017-04-07 20:15:07 +0300

    first commit

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,22 @@
 import json
-
+import sys
+import os.path
+from pprint import pprint
 
 def load_data(filepath):
-    pass
-
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r', encoding='utf-8', errors='ignore') as json_file:
+        return json.load(json_file)
 
 def pretty_print_json(data):
-    pass
+    pprint(data) 
 
 
 if __name__ == '__main__':
-    pass
+    fpath = sys.argv[1]
+    pretty_print_json(load_data(fpath))
+
+
+
+
